80cb3c01be61c432f91fc4a07b0d997213216354
tracer-ysyx <tracer@ysyx.org>
2025-01-08 14:27:18 +0800

    > sim RTL ysyx_202401202056 Qing Linux furina-virtual-machine 6.8.0-50-generic #51~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 21 12:03:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:27:18 up 12 min,  1 user,  load average: 0.02, 0.16, 0.21

---
+++ npc/verilator_example/obj_dir/Vdouble_sw___024root__Slow.cpp
@@ -0,0 +1,25 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vdouble_sw.h for the primary calling header
+
+#include "verilated.h"
+
+#include "Vdouble_sw__Syms.h"
+#include "Vdouble_sw___024root.h"
+
+void Vdouble_sw___024root___ctor_var_reset(Vdouble_sw___024root* vlSelf);
+
+Vdouble_sw___024root::Vdouble_sw___024root(Vdouble_sw__Syms* symsp, const char* v__name)
+    : VerilatedModule{v__name}
+    , vlSymsp{symsp}
+ {
+    // Reset structure values
+    Vdouble_sw___024root___ctor_var_reset(this);
+}
+
+void Vdouble_sw___024root::__Vconfigure(bool first) {
+    if (false && first) {}  // Prevent unused
+}
+
+Vdouble_sw___024root::~Vdouble_sw___024root() {
+}
---
+++ npc/verilator_example/sim_main.cpp
@@ -0,0 +1,38 @@
+#include"Vdouble_sw.h"
+#include"verilated.h"
+#include"stdio.h"
+#include"stdlib.h"
+#include"assert.h"
+
+#include"verilated_vcd_c.h" //if export vcd,and use it
+
+int main(int argc,char** argv)
+{
+	VerilatedContext* contextp=new VerilatedContext;//create a new Verilated context object
+	contextp->commandArgs(argc,argv);//set command line parameters to context
+	Vdouble_sw* top=new Vdouble_sw{contextp};
+
+	VerilatedVcdC* tfp=new VerilatedVcdC;//init VCD*
+	contextp->traceEverOn(true);// trace function begin
+	top->trace(tfp,0);
+	tfp->open("wave.vcd");//set output file'wave.vcd'
+	while(!contextp->gotFinish())
+	{
+		int a=rand() & 1;
+		int b=rand() &1;
+		top->a=a;
+		top->b=b;
+		top->eval();
+		printf("a = %d,b = %d,f = %d\n",a,b,top->f);
+		
+		tfp->dump(contextp->time());//dump wave
+		contextp->timeInc(1);//push simulation time
+		assert(top->f == (a^b));
+	}
+	delete top;
+	tfp->close();
+	delete contextp;
+	return 0;
+}
+
+	
