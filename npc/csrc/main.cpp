3aa34a5e2a0922a9cada2312a78ef5422ba25bd5
tracer-ysyx <tracer@ysyx.org>
2025-01-11 16:59:36 +0800

    > sim RTL ysyx_202401202056 Qing Linux furina-virtual-machine 6.8.0-50-generic #51~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 21 12:03:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:59:36 up  2:44,  1 user,  load average: 0.78, 0.34, 0.25

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,16 +1,31 @@
 #include <nvboard.h>
 #include <Vdouble_sw.h>
  
-static TOP_NAME dut;
- 
-void nvboard_bind_all_pins(TOP_NAME* double_sw);
- 
+
+static TOP_NAME Vdut;
+
+void nvboard_bind_all_pins(TOP_NAME* top);
+
+//static void single_cycle() {
+ // Vdut.clk = 0; Vdut.eval();
+ // Vdut.clk = 1; Vdut.eval();
+//}
+
+//static void reset(int n) {
+  //dut.rst = 1;
+  //while (n -- > 0) single_cycle();
+  //dut.rst = 0;
+//}
+
 int main() {
-  nvboard_bind_all_pins(&dut);
+  nvboard_bind_all_pins(&Vdut);
   nvboard_init();
- 
+
+  //reset(10);
+
   while(1) {
     nvboard_update();
-    dut.eval(); 
+    Vdut.eval();
+    //single_cycle();
   }
 }
