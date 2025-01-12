401fefcdf66cd299614dbd0de120eb7dc893923a
tracer-ysyx <tracer@ysyx.org>
2025-01-12 11:09:57 +0800

    > sim RTL ysyx_202401202056 Qing Linux furina-virtual-machine 6.8.0-50-generic #51~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 21 12:03:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:09:57 up  1:51,  1 user,  load average: 0.07, 0.12, 0.14

--- npc/vsrc/seg.v
+++ npc/vsrc/seg.v
@@ -4,14 +4,14 @@ module seg(
 );
     always @(*) begin
         case(bin_in)
-            3'b000:seg_out=7'b1111110;
-            3'b001:seg_out=7'b0110000;
-            3'b010:seg_out=7'b1101101;
-            3'b011:seg_out=7'b1111001;
-            3'b100:seg_out=7'b0110011;
-            3'b101:seg_out=7'b1011011;
-            3'b110:seg_out=7'b0011111;
-            3'b111:seg_out=7'b1110000;
+            3'b000:seg_out=7'b0000001;
+            3'b001:seg_out=7'b1001111;
+            3'b010:seg_out=7'b0010010;
+            3'b011:seg_out=7'b0000110;
+            3'b100:seg_out=7'b1001100;
+            3'b101:seg_out=7'b0100100;
+            3'b110:seg_out=7'b1100000;
+            3'b111:seg_out=7'b0001111;
         endcase
     end
 endmodule
