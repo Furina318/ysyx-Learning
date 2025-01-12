f768343abd1bf1dba6ec60f32622fb37f1af5b5c
tracer-ysyx <tracer@ysyx.org>
2025-01-12 16:49:29 +0800

    > sim RTL ysyx_202401202056 Qing Linux furina-virtual-machine 6.8.0-50-generic #51~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 21 12:03:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:49:29 up  3:18,  1 user,  load average: 0.07, 0.29, 0.27

--- npc/vsrc/lfsr_num.v
+++ npc/vsrc/lfsr_num.v
@@ -6,18 +6,21 @@ module lfsr_num(
     output reg [7:0] num1
 );
 reg [7:0] q;
+reg [31:0] count;
 always @(posedge clk or posedge rst or posedge stop) begin
     if(rst) begin
         q[7:0]<=8'b00000001;
+        count<=0;
     end
     else begin
         if(stop) begin
             q[7:0]<=q[7:0];
         end
-        else begin
+        if(count==0) begin
             q[6:0]<=q[7:1];
             q[7]<=q[4]^q[3]^q[2]^q[0];
         end
+        count<=(count>=5000000 ? 32'b0 : count+1);
     end
 end
 
