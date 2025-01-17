5fafecc3916b8f0abf4cf025e2ba9e636226c02f
tracer-ysyx <tracer@ysyx.org>
2025-01-17 18:53:04 +0800

    > compile NEMU ysyx_202401202056 Qing Linux furina-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:53:04 up 31 min,  1 user,  load average: 0.00, 0.03, 0.08

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -180,7 +180,7 @@ static int find_main_operator(int p,int q){
     }
     if (cnt>0) continue;
     if(tokens[i].type!=TK_NUM && tokens[i].type!=TK_NOTYPE && tokens[i].type!=TK_EQ && tokens[i].type!=TK_NEQ){//排除非运算符
-      if(high_operator_level<operator_level(i)){
+      if(high_operator_level<=operator_level(i)){
         high_operator_level=operator_level(i);
         main_operator=i;
       }
