0585311b4289c365778a2362e49db1c4d593330a
tracer-ysyx <tracer@ysyx.org>
2025-01-18 15:39:47 +0800

    > compile NEMU ysyx_202401202056 Qing Linux furina-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:39:47 up  4:11,  1 user,  load average: 0.00, 0.04, 0.09

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -128,14 +128,14 @@ static bool make_token(char *e) {
             tokens[nr_token].type=rules[i].token_type;
             nr_token++;
             break;
-          case '-': // 处理一元和二元减法
+          case '-': // 处理一元和多元减法
             if (nr_token == 0 || tokens[nr_token - 1].type == '(' ||
                 tokens[nr_token - 1].type == '+' || tokens[nr_token - 1].type == '-' ||
                 tokens[nr_token - 1].type == '*' || tokens[nr_token - 1].type == '/' ||
-                tokens[nr_token - 1].type == TK_NEG) { // 新增条件：前面也是负号
-              tokens[nr_token].type = TK_NEG; // 标记为一元运算符
+                tokens[nr_token - 1].type == TK_NEG) { 
+              tokens[nr_token].type = TK_NEG; 
             } else {
-              tokens[nr_token].type = '-'; // 标记为二元运算符
+              tokens[nr_token].type = '-'; 
             }
             strncpy(tokens[nr_token].str, substr_start, substr_len);
             tokens[nr_token].str[substr_len] = '\0';
@@ -226,8 +226,7 @@ word_t eval(int p,int q){
       return -1;
     }
     if (tokens[op].type == TK_NEG) {
-      int neg_count = 0;
-      // 计算连续的一元减号数量
+      int neg_count = 0;// 计算连续的一元减号数量
       for (int j = op; j <= q && tokens[j].type == TK_NEG; j++, neg_count++);
       word_t val = eval(op + neg_count, q); // 跳过所有的一元减号
       return (neg_count % 2 == 0) ? val : -val; // 根据奇偶性决定最终是加还是减
