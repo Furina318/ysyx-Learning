cdef0466f7947c94f462a5f3aa5715fd6c61e581
tracer-ysyx <tracer@ysyx.org>
2025-01-18 20:56:15 +0800

    > compile NEMU ysyx_202401202056 Qing Linux furina-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:56:15 up  8:30,  1 user,  load average: 0.00, 0.14, 0.19

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -199,7 +199,7 @@ static bool make_token(char *e) {
             long reg_val = isa_reg_str2val(reg_name);
             printf("%ld\n",reg_val);
             if (reg_val < 0) {
-              printf("未知寄存器名: %s\n", reg_name);
+              printf("Unknow reg name: %s\n", reg_name);
               return false;
             }
             // 将寄存器值转换为十进制字符串并存储到 tokens 中
