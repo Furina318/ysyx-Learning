dc818013a20ead3c8028113bbba7387612ca49da
tracer-ysyx <tracer@ysyx.org>
2025-01-19 09:16:15 +0800

    > compile NEMU ysyx_202401202056 Qing Linux furina-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:16:15 up 12 min,  1 user,  load average: 0.28, 0.28, 0.28

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -247,7 +247,6 @@ static bool check_parentheses(int p,int q){
 static int operator_level(int op_type){
   switch(tokens[op_type].type){
     case TK_NEG: return 5;
-    case TK_PO: return 5;
     case '+': return 4;
     case '-': return 4;
     case '*': return 3;
@@ -256,7 +255,7 @@ static int operator_level(int op_type){
     case TK_NEQ:return 2;
     case TK_AND:return 1;
     default:  
-      printf("Undefine oprator\n");
+      //printf("Undefine oprator\n");
       // assert(0);
       return -1;
   }
