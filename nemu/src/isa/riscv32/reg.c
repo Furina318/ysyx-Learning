3c454fcfcbe3ecc3307bb2fc64b86752da845296
tracer-ysyx <tracer@ysyx.org>
2025-01-19 18:38:29 +0800

    > compile NEMU ysyx_202401202056 Qing Linux furina-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:38:29 up 3 min,  1 user,  load average: 2.38, 1.94, 0.83

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -27,7 +27,7 @@ const char *regs[] = {
 void isa_reg_display() {
   static int i;
   for(i=0;i<sizeof(regs)/sizeof(regs[0]);i++){
-    printf("%-4s : 0x%08x %d\n", regs[i],cpu.gpr[i],cpu.gpr[i]);
+    printf("%-4s : 0x%08x %u\n", regs[i],cpu.gpr[i],cpu.gpr[i]);
   }
   printf("pc   : 0x%08x %d\n",cpu.pc,cpu.pc);
 }
