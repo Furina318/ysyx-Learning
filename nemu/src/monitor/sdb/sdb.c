c1e62896fd04ba12966aba079912d9c7ad29fb31
tracer-ysyx <tracer@ysyx.org>
2025-01-19 17:00:06 +0800

    > compile NEMU ysyx_202401202056 Qing Linux furina-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:00:06 up  5:30,  1 user,  load average: 0.24, 0.15, 0.15

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -134,8 +134,6 @@ static int cmd_w(char *args){
   }
   char *EXPR=args;
   WP *wp = new_wp();
-  printf("get\n");
-  if (wp == NULL) return 0;
   strcpy(wp->expr,EXPR);
   wp->old_val=expr(EXPR); 
   return 0;
