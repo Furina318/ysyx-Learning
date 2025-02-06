dfbdb0a23f4167ba4d667499fbaf1c5196b76247
tracer-ysyx <tracer@ysyx.org>
2025-02-06 15:54:13 +0800

    > compile NEMU ysyx_202401202056 Qing Linux furina-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:54:13 up  3:01,  1 user,  load average: 0.02, 0.07, 0.12

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -138,7 +138,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst, ilen);
 
-  iringbuf_push(s->pc,s->isa.inst,s->logbuf);
+  iringbuf_push(cpu.pc,s->isa.inst,s->logbuf);
 #endif
 }
 
