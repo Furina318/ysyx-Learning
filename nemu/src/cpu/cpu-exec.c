8de7c7a2a8abeb46e6b09a04fd8961fdf425e077
tracer-ysyx <tracer@ysyx.org>
2025-02-06 15:57:49 +0800

    > compile NEMU ysyx_202401202056 Qing Linux furina-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:57:49 up  3:05,  1 user,  load average: 0.07, 0.05, 0.09

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -63,7 +63,7 @@ void iringbuf_push(vaddr_t pc,uint32_t inst,char *logbuf){
 void iringbuf_dummy(vaddr_t error_pc){
   printf("Recent inst\n");
   int count=iringbuf.full ? IRINGBUF_SIZE : iringbuf.w_ptr;
-  for(int i=1;i<=count;i++){
+  for(int i=0;i<count;i++){
     int index=(iringbuf.r_ptr+i)%IRINGBUF_SIZE;
     if(iringbuf.entries[index].pc==error_pc) printf(" --> ");
     else printf("     ");
@@ -151,7 +151,7 @@ static void execute(uint64_t n) {
     if (nemu_state.state != NEMU_RUNNING) break;
     IFDEF(CONFIG_DEVICE, device_update());
   }
-  if(nemu_state.state==NEMU_END){//程序出错时
+  if(nemu_state.state==NEMU_ABORT){//程序出错时
     vaddr_t error_pc=cpu.pc;
     iringbuf_dummy(error_pc);
   }
