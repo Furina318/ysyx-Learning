7a09bce8d95889eadafefd4d202f42f43d00c9ba
tracer-ysyx <tracer@ysyx.org>
2025-02-13 21:35:10 +0800

    > compile NEMU ysyx_202401202056 Qing Linux furina-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:35:10 up 23 min,  1 user,  load average: 0.09, 0.14, 0.21

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -186,7 +186,12 @@ static void exec_once(Decode *s, vaddr_t pc) {
     vaddr_t ret_addr=pc+4;
     char *name=get_func_name(target);
     ftrace_call(pc,name,ret_addr);
-  }else if(s->isa.inst==0x00008067){//RET指令11000 11
+  }else if(opcode==0x67){//JALR指令11001 11
+    if(s->isa.inst==0x00008067){
+      if(ftrace_size>0){
+        ftrace_call(pc,ftrace[ftrace_size-1].name,s->dnpc);
+      }
+    }
     if(ftrace_size>0 && target==ftrace[ftrace_size-1].back){//判断是否为函数返回
       ftrace_ret(pc,ftrace[ftrace_size-1].name);
     }
