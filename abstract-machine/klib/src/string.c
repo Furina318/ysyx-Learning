018cf4d43c1d8758d77bb46abd4f95194249b5bc
tracer-ysyx <tracer@ysyx.org>
2025-02-18 11:40:36 +0800

    > compile NEMU ysyx_202401202056 Qing Linux furina-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:40:36 up  1:08,  1 user,  load average: 0.31, 0.52, 0.38

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -39,7 +39,7 @@ char *strcat(char *dst, const char *src) {
 
 int strcmp(const char *s1, const char *s2) {
   if(s1==NULL || s2==NULL) return 0;
-  while(*s1 && *s2 && *s1==*s2){
+  while(*s1 && *s1==*s2){
     s1++;
     s2++;
   }
