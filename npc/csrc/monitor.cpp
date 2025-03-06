d2d83142297c42ea36d0cc93c567e27a53a279e7
tracer-ysyx <tracer@ysyx.org>
2025-03-06 16:33:41 +0800

    > compile NEMU ysyx_202401202056 Qing Linux furina-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:33:41 up 49 min,  1 user,  load average: 0.28, 0.16, 0.22

--- npc/csrc/monitor.cpp
+++ npc/csrc/monitor.cpp
@@ -45,7 +45,7 @@ static char *elf_file = NULL;
 static char *img_file = NULL;
 static int difftest_port = 1234;
 
-static long load_img() {
+static long load_img() {//load_img函数用于加载镜像文件
   if (img_file == NULL) {
     Log("No image is given. Use the default build-in image.");
     return 4096; // built-in image size
