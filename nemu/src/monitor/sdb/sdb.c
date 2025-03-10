ada2474a8b1fbb15696d7fc8baf0f6aa32fbd5cd
tracer-ysyx <tracer@ysyx.org>
2025-03-10 18:04:39 +0800

    > compile NEMU ysyx_202401202056 Qing Linux furina-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:04:39 up  3:28,  1 user,  load average: 0.41, 0.20, 0.19

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -93,8 +93,9 @@ static int cmd_info(char *args){
       // _Log(ANSI_FG_YELLOW "Current watchpoints state: %s\n" ANSI_NONE, wp_state);
       _Log("Current watchpoints state: ");
       _Log(ANSI_FG_RED "%s\n" ANSI_NONE, wp_state);
+      _Log(ANSI_FG_GREEN "Watchpoint NO\t Expression\t Last Value\n" ANSI_NONE);
       while (wp != NULL) { // 打印监视点信息
-        _Log(ANSI_FG_GREEN "Watchpoint NO:%-2d: Expression '%s' Last Value: 0x%08x\n" ANSI_NONE,
+        _Log("%-2d\t %s\t 0x%08x\n" ANSI_NONE,
           wp->NO, wp->expr ? wp->expr : "N/A", wp->old_val);
         wp = wp->next;
       }
