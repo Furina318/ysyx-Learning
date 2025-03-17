6f69c3ded589d0014df61d9c93322d9f3584220a
tracer-ysyx <tracer@ysyx.org>
2025-03-18 00:46:05 +0800

    > compile NEMU ysyx_202401202056 Qing Linux furina-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:46:05 up 54 min,  1 user,  load average: 0.50, 0.19, 0.11

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -9,7 +9,7 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t kc = inl(KBD_ADDR);//从键盘设备读取32位数据
   kbd->keydown = kc & KEYDOWN_MASK ? true : false;
   kbd->keycode = kc & ~KEYDOWN_MASK;
-  if (kbd->keycode == 0 && !kbd->keydown) {
-    kbd->keycode = AM_KEY_NONE; // 设置为无效键码
-  }
+  // if (kbd->keycode == 0 && !kbd->keydown) {
+  //   kbd->keycode = AM_KEY_NONE; // 设置为无效键码
+  // }
 }
