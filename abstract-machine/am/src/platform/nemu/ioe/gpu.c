ded82238cb5512ddfe8e173c66912f54dc587369
tracer-ysyx <tracer@ysyx.org>
2025-03-19 14:22:39 +0800

    > compile NEMU ysyx_202401202056 Qing Linux furina-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:22:39 up  1:04,  1 user,  load average: 0.86, 0.76, 0.52

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -27,21 +27,19 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {//屏幕大小寄存器软件实现�
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  int x=ctl->x, y=ctl->y, w=ctl->w, h=ctl->h;
-  if(!ctl->sync && (w==0 || h==0)) return;//如果 sync 为 false 且 w 和 h 都为 0，则直接返回，不进行绘图操作
-
-  // 图像像素按行优先方式存储在pixels，其对应索引index=(row*w+col)
-  //先存储第一行的所有像素，再存储第二行的所有像素，依此类推
-  uint32_t *pixels=ctl->pixels;//跟fb同类型才可以进行赋值
-  uint32_t *fb=(uint32_t *)(uintptr_t)FB_ADDR;
-  uint32_t get_w=inl(VGACTL_ADDR)>>16;
-  for(int i=y;i<y+h;i++){//行遍历
-    for(int j=x;j<x+w;j++){//列遍历，实际就是遍历每一行中各个元素然后进入下一行
-      fb[i*get_w+j]=pixels[(i-y)*get_w+(j-x)];
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  if (!ctl->sync && (w == 0 || h == 0))
+    return;
+  uint32_t *pixels = ctl->pixels;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t screen_w = inl(VGACTL_ADDR) >> 16;
+  for (int i = y; i < y+h; i++) {
+    for (int j = x; j < x+w; j++) {
+      fb[screen_w*i+j] = pixels[w*(i-y)+(j-x)]; //缓冲区是一个像素块
     }
   }
   if (ctl->sync) {
-    outl(SYNC_ADDR, 1);//如果 sync 为 true，则向 SYNC_ADDR 地址写入 1，触发屏幕同步操作
+    outl(SYNC_ADDR, 1);    //将sync置1，nemu会进行屏幕更新
   }
 }
 
