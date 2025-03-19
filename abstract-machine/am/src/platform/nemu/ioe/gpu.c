2167e63586d451a8df3498343a442fc83717bee0
tracer-ysyx <tracer@ysyx.org>
2025-03-19 15:06:32 +0800

    > compile NEMU ysyx_202401202056 Qing Linux furina-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:06:32 up  1:48,  1 user,  load average: 0.16, 0.37, 0.35

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -35,9 +35,10 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   uint32_t *pixels=ctl->pixels;//跟fb同类型才可以进行赋值
   uint32_t *fb=(uint32_t *)(uintptr_t)FB_ADDR;
   uint32_t get_w=inl(VGACTL_ADDR)>>16;
+
   for(int i=y;i<y+h;i++){//行遍历
     for(int j=x;j<x+w;j++){//列遍历，实际就是遍历每一行中各个元素然后进入下一行
-      fb[i*get_w+j]=pixels[(i-y)*w+(j-x)];
+      fb[i*get_w+j]=pixels[(i-y)*w+(j-x)];//此处计算pixels要注意用提取的w，不是用屏幕的宽度get_w
     }
   }
   if (ctl->sync) {
