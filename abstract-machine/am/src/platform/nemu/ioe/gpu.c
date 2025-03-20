90f879e6bf60c3c32d56d7a3727060d918855e59
tracer-ysyx <tracer@ysyx.org>
2025-03-20 15:27:50 +0800

    > compile NEMU ysyx_202401202056 Qing Linux furina-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:27:50 up  3:48,  1 user,  load average: 0.93, 0.50, 0.41

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -27,18 +27,20 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {//屏幕大小寄存器软件实现�
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  int x=ctl->x, y=ctl->y, w=ctl->w, h=ctl->h;
+  int x=ctl->x, y=ctl->y, w=ctl->w, h=ctl->h; // 获取绘图区域的起始坐标(x,y)和尺寸(w,h)
   if(!ctl->sync && (w==0 || h==0)) return;//如果 sync 为 false 且 w 和 h 都为 0，则直接返回，不进行绘图操作
 
   // 图像像素按行优先方式存储在pixels，其对应索引index=(row*w+col)
   //先存储第一行的所有像素，再存储第二行的所有像素，依此类推
   uint32_t *pixels=ctl->pixels;//跟fb同类型才可以进行赋值
-  uint32_t *fb=(uint32_t *)(uintptr_t)FB_ADDR;
-  uint32_t get_w=inl(VGACTL_ADDR)>>16;
+  uint32_t *fb=(uint32_t *)(uintptr_t)FB_ADDR;//将帧缓冲区地址转换为uint32_t指针类型
+  uint32_t get_w=inl(VGACTL_ADDR)>>16;//从VGACTL_ADDR读取屏幕宽度，右移16位获取高16位的宽度值
 
-  for(int i=y;i<y+h;i++){//行遍历
-    for(int j=x;j<x+w;j++){//列遍历，实际就是遍历每一行中各个元素然后进入下一行
-      fb[i*get_w+j]=pixels[(i-y)*w+(j-x)];//此处计算pixels要注意用提取的w，不是用屏幕的宽度get_w
+  for(int i=y;i<y+h;i++){//行遍历，从起始行y开始，遍历h行
+    for(int j=x;j<x+w;j++){//列遍历，从起始列x开始，遍历w列
+      fb[i*get_w+j]=pixels[(i-y)*w+(j-x)];//将像素数据从源缓冲区复制到帧缓冲区
+      //fb中的位置：当前行i乘以屏幕宽度get_w，再加上当前列j
+      //pixels中的位置：相对行(i-y)乘以图像宽度w，再加上相对列(j-x)
     }
   }
   if (ctl->sync) {
