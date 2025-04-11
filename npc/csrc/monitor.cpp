e80b29fe0405f957bc3fa0d28990c186d27d1f92
tracer-ysyx <tracer@ysyx.org>
2025-04-11 08:58:09 +0800

    > compile NEMU ysyx_22040000 张三 Linux furina-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:58:09 up 4 min,  1 user,  load average: 0.70, 1.05, 0.53

--- npc/csrc/monitor.cpp
+++ npc/csrc/monitor.cpp
@@ -228,7 +228,6 @@ void init_monitor(int argc, char *argv[]) {
     printf("Get difftest file : %s\n",diff_so_file);
     init_difftest(diff_so_file, img_size, difftest_port);
   }
-  printf("Yes\n");
 #endif
   /* Initialize the simple debugger. */
   init_sdb();
