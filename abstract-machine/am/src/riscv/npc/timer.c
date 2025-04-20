288aae0143b2c8b86f00c0b3f77a0d00d2828108
tracer-ysyx <tracer@ysyx.org>
2025-04-21 00:24:05 +0800

    > compile NEMU ysyx_22040000 张三 Linux furina-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:24:05 up  2:12,  1 user,  load average: 0.61, 0.56, 0.30

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -1,10 +1,17 @@
 #include <am.h>
+#include "../npc/include/npc.h"
+#include "../riscv.h"
 
 void __am_timer_init() {
+  outl(RTC_ADDR,0);//将计时器的低 32 位和高 32 位清零
+  outl(RTC_ADDR + 4,0);
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  // uptime->us = 0;
+  uptime->us = (uint64_t)inl(RTC_ADDR+4);//使用inl函数从RTC_ADDR和RTC_ADDR + 4读取计时器的低 32 位和高 32 位
+  uptime->us <<= 32;
+  uptime->us += (uint64_t)inl(RTC_ADDR);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
