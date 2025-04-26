5a94f157505177dcd2e11507112ae90a10d27569
tracer-ysyx <tracer@ysyx.org>
2025-04-26 13:39:04 +0800

    > compile NEMU ysyx_22040000 张三 Linux furina-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:39:04 up 19 min,  1 user,  load average: 0.12, 0.28, 0.38

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -31,6 +31,7 @@ void init_disasm();
 
 static void welcome() {
   Log("Trace: %s", MUXDEF(CONFIG_TRACE, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));
+  Log("Etrace: %s", MUXDEF(CONFIG_E_TRACE, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));
   IFDEF(CONFIG_TRACE, Log("If trace is enabled, a log file will be generated "
         "to record the trace. This may lead to a large log file. "
         "If it is not necessary, you can disable it in menuconfig"));
