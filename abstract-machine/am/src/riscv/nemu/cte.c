199dc8388865680f68ba765e58382ce4ebbbf4c9
tracer-ysyx <tracer@ysyx.org>
2025-04-26 23:40:07 +0800

    > compile NEMU ysyx_22040000 张三 Linux furina-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:40:07 up  1:30,  1 user,  load average: 0.04, 0.24, 0.23

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,7 +9,7 @@ Context* __am_irq_handle(Context *c) {
     Event ev = {0};
     // printf("%d\n",c->mcause);
     switch (c->mcause) {
-      case -1:  ev.event = EVENT_YIELD; c->mepc += 4;break;
+      case -1:  ev.event = EVENT_YIELD; break;
       default: ev.event = EVENT_ERROR; break;
     }
     //user_handler是一个函数指针，指向一个函数，该函数接受一个Event类型的参数和一个Context类型的参数，并返回一个Context类型的指针.
