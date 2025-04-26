90690e76beed851910af8fe4fa6b2f9e925c3e30
tracer-ysyx <tracer@ysyx.org>
2025-04-26 14:51:40 +0800

    > compile NEMU ysyx_22040000 张三 Linux furina-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:51:40 up  1:32,  1 user,  load average: 0.63, 0.49, 0.35

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -36,7 +36,7 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  Context *cp = (Context *)(kstack.end - sizeof(Context));
+  Context *cp = (Context *)(kstack.end - sizeof(Context *));
   cp->mepc = (uintptr_t)entry;
   return cp;
 }
