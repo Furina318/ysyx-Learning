217a5e9eaf38ed8464749df94ce34c52bd944f3e
tracer-ysyx <tracer@ysyx.org>
2025-04-27 14:34:54 +0800

    > compile NEMU ysyx_22040000 张三 Linux furina-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:34:54 up  1:19,  1 user,  load average: 0.39, 0.24, 0.20

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -434,7 +434,7 @@ void cpu_exec(uint64_t n) {
   uint64_t timer_start = get_time();
 
   execute(n);
-  cpu.csr.mcause=0xb;
+
   uint64_t timer_end = get_time();
   g_timer += timer_end - timer_start;
 
