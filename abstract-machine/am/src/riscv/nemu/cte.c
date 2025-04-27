fd1f45fc1a473c1e62356a0de8b1abb09d0776ca
tracer-ysyx <tracer@ysyx.org>
2025-04-27 15:49:12 +0800

    > compile NEMU ysyx_22040000 张三 Linux furina-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:49:12 up 38 min,  1 user,  load average: 0.84, 0.53, 0.50

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,28 +7,16 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
-
-		/*
+    // printf("%d\n",c->mcause);
     switch (c->mcause) {
+      case 11:  ev.event = EVENT_YIELD; c->mepc += 4;break;
       default: ev.event = EVENT_ERROR; break;
     }
-		*/
-		if(c->mcause == 11) {
-			if(c->GPR1 == -1) {
-				ev.event = EVENT_YIELD;
-				c->mepc += 4;
-			} else if(c->GPR1 >= 0 && c->GPR1 <= 19) {
-				ev.event = EVENT_SYSCALL;
-				c->mepc += 4;
-			} else {
-				ev.event = EVENT_ERROR;
-				}
-		}
-
+    //user_handler是一个函数指针，指向一个函数，该函数接受一个Event类型的参数和一个Context类型的参数，并返回一个Context类型的指针.
+    //是cte_init函数中注册的异常处理函数
     c = user_handler(ev, c);
     assert(c != NULL);
   }
-	//printf("Context: mepc=%d\n",c->mepc);
 
   return c;
 }
@@ -36,7 +24,9 @@ Context* __am_irq_handle(Context *c) {
 extern void __am_asm_trap(void);
 
 bool cte_init(Context*(*handler)(Event, Context*)) {
-  // initialize exception entry
+  // initialize exception entry       内联汇编，异常处理的入口地址设置为__am_asm_trap
+  //%0是内联汇编中的操作数占位符，他表示内联汇编指令中的第一个操作数。在这里的内联汇编指令中，%0用来引用第一个输入操作数，即"r"(__am_asm_trap)中的__am_asm_trap
+  //"r"约束表示将一个寄存器作为输入操作数
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
 
   // register event handler
@@ -46,12 +36,9 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-	Context *cp = (Context *)(kstack.end - sizeof(Context));
-	cp->mepc = (uintptr_t)entry;
-	cp->gpr[10] = (uintptr_t)(arg);
-	
-	return cp;
- // return NULL;
+  Context *cp = (Context *)(kstack.end - sizeof(Context *));
+  cp->mepc = (uintptr_t)entry;
+  return cp;
 }
 
 void yield() {
@@ -61,7 +48,12 @@ void yield() {
   asm volatile("li a7, -1; ecall");
 #endif
 }
-
+// 整个 yield 操作的流程如下：​
+// 应用程序调用 yield()，触发 ecall 指令。
+// NEMU 模拟器识别 ecall，保存上下文，并跳转到异常处理程序。
+// AM 层的异常处理程序构造事件，并调用用户注册的事件处理器。
+// 事件处理器根据事件类型执行相应操作，如任务切换。
+// 恢复上下文，执行 mret 指令，返回用户程序
 bool ienabled() {
   return false;
 }
