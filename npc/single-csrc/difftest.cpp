0f75452e47c22330015125afcc0f20960d09f488
tracer-ysyx <tracer@ysyx.org>
2025-05-08 21:50:13 +0800

    > compile NEMU ysyx_22040000 张三 Linux furina-virtual-machine 6.8.0-58-generic #60~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 28 16:09:21 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:50:13 up  1:01,  1 user,  load average: 0.23, 0.18, 0.22

--- npc/single-csrc/difftest.cpp
+++ npc/single-csrc/difftest.cpp
@@ -177,7 +177,6 @@ void difftest_step(vaddr_t pc, vaddr_t npc)
         ref_difftest_exec(1);
         ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
 
-        printf("mcause:0x%08x\n",ref_r.csr.mcause);
         checkregs(&ref_r, pc, npc);
     }
 }
