1f23fbc04e85e2820f8bbb443f7d276918e6d7cf
tracer-ysyx <tracer@ysyx.org>
2025-05-23 16:59:46 +0800

    > compile NEMU ysyx_22040000 张三 Linux furina-virtual-machine 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:59:46 up 48 min,  1 user,  load average: 0.85, 0.71, 0.55

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -253,9 +253,11 @@ static void exec_once(Decode *s, vaddr_t pc) {
   s->pc = pc;
   s->snpc = pc;
   isa_exec_once(s);
+
+  uint32_t opcode = s->isa.inst & 0x7f;
 #ifdef CONFIG_BRANCH_Predictor
   // 分支预测器逻辑
-  uint32_t opcode = s->isa.inst & 0x7f;
+  // uint32_t opcode = s->isa.inst & 0x7f;
   bool is_branch = (opcode == 0x63);  //条件分支 (beq,bne等)
   bool is_jal = (opcode == 0x6f);     //无条件跳转(jal)
   bool is_jalr = (opcode == 0x67);    //间接跳转(jalr)
