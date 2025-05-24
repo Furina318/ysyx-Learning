b3def8fecb82a80edce227b1bb18d2b06ba99907
tracer-ysyx <tracer@ysyx.org>
2025-05-24 12:33:00 +0800

    > compile NEMU ysyx_22040000 张三 Linux furina-virtual-machine 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:33:00 up 26 min,  1 user,  load average: 0.14, 0.31, 0.33

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -93,17 +93,15 @@ void *memcpy(void *out, const void *in, size_t n) {
   char *dst=(char *)out;
   const char *src=(const char *)in;
   // while(n--) *dst++ = *src++;
-  if (src < dst && dst < src + n) {
-        // 从后往前复制防止覆盖
+  if (src < dst && dst < src + n) {// 如果内存区域重叠，且目标地址在源地址之前
         dst += n - 1;
         src += n - 1;
         while (n--) {
             *dst-- = *src--;
         }
     } else {
-        // 正常从前向后复制
         while (n--) {
-            *dst++ = *src++;
+            *dst++ = *src++;//正向复制
         }
     }
     return out;
