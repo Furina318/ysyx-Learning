6f4de92cb9997b01c79a8cbaae0e2e58db7a4356
tracer-ysyx <tracer@ysyx.org>
2025-05-26 00:04:19 +0800

    > compile NEMU ysyx_22040000 张三 Linux furina-virtual-machine 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:04:19 up  1:57,  1 user,  load average: 0.44, 0.52, 0.43

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -56,8 +56,8 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
   }
 }
 
-#define ECALL(dnpc) {IFDEF(CONFIG_E_TRACE,printf("\033[32m[ecall]: dnpc = 0x%08x\033[0m\n",isa_raise_intr(11,s->pc)));\
-   dnpc = (isa_raise_intr(11,s->pc));\
+#define ECALL(dnpc) {IFDEF(CONFIG_E_TRACE,printf("\033[32m[ecall]: dnpc = 0x%08x\033[0m\n",isa_raise_intr(8,s->pc)));\
+   dnpc = (isa_raise_intr(8,s->pc));\
 }
 
 static int decode_exec(Decode *s) {
