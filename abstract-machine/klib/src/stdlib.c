fb0606297d8a289387d393d62519525987a1c0ab
tracer-ysyx <tracer@ysyx.org>
2025-05-26 21:05:37 +0800

    > compile NEMU ysyx_22040000 张三 Linux furina-virtual-machine 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:05:37 up 7 min,  1 user,  load average: 0.37, 0.60, 0.37

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -1,27 +1,27 @@
-
-
 #include <am.h>
 #include <klib.h>
 #include <klib-macros.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
+static bool flag=false;
+static char *start_addr;
 
-extern int rand(void) {
+int rand(void) {
   // RAND_MAX assumed to be 32767
   next = next * 1103515245 + 12345;
   return (unsigned int)(next/65536) % 32768;
 }
 
-extern void srand(unsigned int seed) {
+void srand(unsigned int seed) {
   next = seed;
 }
 
-extern int abs(int x) {
+int abs(int x) {
   return (x < 0 ? -x : x);
 }
 
-extern int atoi(const char* nptr) {
+int atoi(const char* nptr) {
   int x = 0;
   while (*nptr == ' ') { nptr ++; }
   while (*nptr >= '0' && *nptr <= '9') {
@@ -31,43 +31,24 @@ extern int atoi(const char* nptr) {
   return x;
 }
 
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-#endif
-
-char *hbrk;
-
-static void initialize() {
-	hbrk = heap.start;
-}
-
-extern void *malloc(size_t size) {
-	if(hbrk == NULL) {
-		initialize();
-	}
-  
-  size  = (size_t)ROUNDUP(size, 4);
-  char *old = hbrk;
-  hbrk += size;
-  return old;
+void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-	
-		/*
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  //panic("Not implemented");
-  size  = (size_t)ROUNDUP(size, 4);
-  char *old = hbrk;
-  hbrk += size;
-  return old;
+#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))//如果没有定义...或者用了定义...那么使用自定义malloc否则用库函数
+  if(!flag){//如果是第一次使用，需要进行初始化
+    start_addr=(void*)ROUNDUP(heap.start,8);//将start_addr初始化为堆的起始位置，并通过宏定义拓展字节(需要对齐)
+    flag=true;
+  }
+  size=(size_t)ROUNDUP(size,8);//将要申请的内存区域拓展到8字节边界
+  char *get_start_addr = start_addr;
+  start_addr += size;//指向下一个可用内存块的地址
+  // panic("Not implemented");
 #endif
-*/
-		//printf("size = %d, addr->size = %d, addr = %d\n");
-		//printf("Wrong malloc!\n");
-  return NULL;
+  return get_start_addr;
 }
 
-extern void free(void *ptr) {
+void free(void *ptr) {
 }
 
 #endif
