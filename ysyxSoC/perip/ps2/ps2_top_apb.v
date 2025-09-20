b40f24d5d9ce836b76cf882dfbc60d9212d6c972
tracer-ysyx <tracer@ysyx.org>
2025-09-20 21:37:56 +0800

    > sim RTL ysyx_25010030 廖宇熙 Linux furina-virtual-machine 6.8.0-83-generic #83~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  9 18:19:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:37:56 up 11 min,  1 user,  load average: 0.88, 0.87, 0.52

--- ysyxSoC/perip/ps2/ps2_top_apb.v
+++ ysyxSoC/perip/ps2/ps2_top_apb.v
@@ -16,80 +16,71 @@ module ps2_top_apb(
   input         ps2_data
 );
 
-  reg [1:0] ps2_state;
-  localparam PS2_IDLE = 0;
-  localparam PS2_READ = 1;
+  reg [ 2:0] ps2_clk_sync;  
+  reg [ 9:0] buffer;
+  reg [ 3:0] counter;
+  reg [ 7:0] fifo[15:0];
+  reg [ 3:0] w_ptr, r_ptr;
+  reg        empty;
+  reg [PS2_STATE_WIDTH-1:0] ps2_apb_state;
 
-  reg [9:0] buffer;      //起始位+8数据位+奇偶位+停止位
-  reg [3:0] counter;
-  reg [7:0] fifo [15:0];
-  reg [3:0] w_ptr;
-  reg [3:0] r_ptr;
-  reg       isn_empty;
-  integer i;
-
-  assign in_pready  = (ps2_state == PS2_READ) ? 1'b1 : 1'b0;
-  assign in_prdata  = (ps2_state == PS2_READ) ? (isn_empty ? {24'd0, fifo[r_ptr]} : 0) : 0;
-  assign in_pslverr = 1'b0;
-
-  //ps2_clk是异步信号，避免亚稳态
-  reg [2:0] ps2_clk_sync;
   always @(posedge clock) begin
     ps2_clk_sync <= {ps2_clk_sync[1:0], ps2_clk};
   end
-  wire sampling = ps2_clk_sync[2] & ~ps2_clk_sync[1];//ps2时钟上升沿触发数据采样
+  wire sampling = ps2_clk_sync[2] & ~ps2_clk_sync[1];
+
+  localparam                       PS2_STATE_WIDTH = 2;
+  localparam [PS2_STATE_WIDTH-1:0] PS2_APB_IDLE    = 2'b0;
+  localparam [PS2_STATE_WIDTH-1:0] PS2_APB_READ    = 2'b1;
+
+  assign in_pready  = (ps2_apb_state == PS2_APB_READ) ? 1'b1 : 1'b0;
+  assign in_prdata  = (ps2_apb_state == PS2_APB_READ) ? (empty ? {24'd0, fifo[r_ptr]} : 32'b0) : 32'b0;
+  assign in_pslverr = 1'b0;
 
   always @(posedge clock or posedge reset) begin
-    if(reset) begin
-      ps2_state <= PS2_IDLE;
-    end
-    else begin
-      case(ps2_state)
-        PS2_IDLE: begin
-          if(in_psel && !in_pwrite) begin
-            ps2_state <= PS2_READ;
-          end
-        end
-        PS2_READ: begin
-          ps2_state <= PS2_IDLE;
-        end
-        default: begin
-          ps2_state <= PS2_IDLE;
-        end
+    if (reset) begin
+      ps2_apb_state <= PS2_APB_IDLE;
+    end else begin
+      case (ps2_apb_state)
+        PS2_APB_IDLE: begin ps2_apb_state <= (in_psel && !in_pwrite) ? PS2_APB_READ : PS2_APB_IDLE; end
+        PS2_APB_READ: begin ps2_apb_state <= PS2_APB_IDLE; end
+        default: begin ps2_apb_state <= PS2_APB_IDLE; end
       endcase
     end
   end
 
-  always @(posedge clock) begin
+  integer i;
+  always @(posedge clock or posedge reset) begin
     if (reset) begin
-      counter   <= 0;
-      w_ptr     <= 0;
-      r_ptr     <= 0;
-      isn_empty <= 0;
-      for (i = 0; i < 8; i++) fifo[i] <= 0;
+      counter <= 4'b0;
+      w_ptr   <= 4'b0;
+      r_ptr   <= 4'b0;
+      empty   <= 1'b0;
+      for (i = 0; i < 8; i++) begin
+        fifo[i] <= 8'b0;
+      end
     end else begin
       if (sampling) begin
         if (counter == 4'd10) begin
-          // 接收完成一帧，做校验
           if ((buffer[0] == 0) && (ps2_data) && (^buffer[9:1])) begin
-            fifo[w_ptr] <= buffer[8:1]; // 保存数据位
+            fifo[w_ptr] <= buffer[8:1];
             w_ptr       <= w_ptr + 1;
-            isn_empty   <= 1;
+            empty       <= 1'b1;
           end
-          counter <= 0;
+          counter <= 4'b0;
         end else begin
           buffer[counter] <= ps2_data;
-          counter         <= counter + 1;
+          counter <= counter + 1;
         end
       end
 
-      // APB 读取时，FIFO 出队
-      if (in_penable & in_pready & isn_empty) begin
-        r_ptr <= r_ptr + 1;
-        if (w_ptr == (r_ptr + 1)) isn_empty <= 0; // FIFO 读空
+      if (in_penable & in_pready & empty) begin
+        r_ptr <= r_ptr + 4'b1;
+        if (w_ptr == (r_ptr + 4'b1)) begin
+          empty <= 1'b0;
+        end
       end
     end
   end
-
 
 endmodule
