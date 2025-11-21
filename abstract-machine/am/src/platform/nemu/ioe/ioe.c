e8dfa3937190edf259161c7fd59b9259c2731ad9
Furina318 <1145676148@qq.com>
2025-11-21 20:43:40 +0800

    nemu增加串口

--- abstract-machine/am/src/platform/nemu/ioe/ioe.c
+++ abstract-machine/am/src/platform/nemu/ioe/ioe.c
@@ -17,10 +17,11 @@ void __am_audio_play(AM_AUDIO_PLAY_T *);
 void __am_disk_config(AM_DISK_CONFIG_T *cfg);
 void __am_disk_status(AM_DISK_STATUS_T *stat);
 void __am_disk_blkio(AM_DISK_BLKIO_T *io);
+void __am_uart_rx(AM_UART_RX_T *);
 
 static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { cfg->present = true; cfg->has_rtc = true; }
 static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true;  }
-static void __am_uart_config(AM_UART_CONFIG_T *cfg)   { cfg->present = false; }
+static void __am_uart_config(AM_UART_CONFIG_T *cfg)   { cfg->present = true; }
 static void __am_net_config (AM_NET_CONFIG_T *cfg)    { cfg->present = false; }
 
 typedef void (*handler_t)(void *buf);
@@ -42,6 +43,7 @@ static void *lut[128] = {
   [AM_DISK_STATUS ] = __am_disk_status,
   [AM_DISK_BLKIO  ] = __am_disk_blkio,
   [AM_NET_CONFIG  ] = __am_net_config,
+  [AM_UART_RX     ] = __am_uart_rx,
 };
 
 static void fail(void *buf) { panic("access nonexist register"); }
